51868dc0597558a8a084934454f28b8705b0e82c
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 12:34:29 +0100

    rebuild

--- js/dist/object.js
+++ js/dist/object.js
@@ -1,6 +1,8 @@
-(function(exports, undefined){
+( function ( ) {
 
-	'use strict';
+'use strict' ;
+
+var definition = function ( exports , undefined ) {
 
 
 /* js/src/deepcopy.js */
@@ -156,4 +158,16 @@ var isequivalent = function ( a , b ) {
 
 exports.isequivalent = isequivalent ;
 
-})(typeof exports === 'undefined' ? this['object'] = {} : exports);
+return exports ;
+} ;
+if ( typeof exports === "object" ) {
+	definition( exports ) ;
+}
+else if ( typeof define === "function" && define.amd ) {
+	define( "@aureooms/js-object" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+}
+else if ( typeof window === "object" && typeof window.document === "object" ) {
+	definition( window["object"] = { } ) ;
+}
+else console.error( "unable to detect type of module to define for @aureooms/js-object") ;
+} )( ) ;
